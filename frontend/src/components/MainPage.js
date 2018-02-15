3ffd74b0432542615aacee149951769495913d3b
Jenner Hanni <jenner@wickerbox.net>
2018-02-15 10:23:50 -0800

    feature: Display posts from server on main page

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -9,43 +9,24 @@ class MainPage extends Component {
   }
 
   render () {
+
     return (
 
 			<section id="content">
-				<article className="post">
-					<h2>The Tree Frog</h2>
-					<p className="post-content">
-						<em>It is not the chambers of the heart that hold him<br />
-						captive, but the hallways of the mind. Why<br />
-						his image burning green and blue persists<br />
-						—the face, the eyes questioning, the shape<br />
-						of his head—is beyond anything I can understand. </em>
-					</p>
-          <p className="post-content">
-						by <a href="https://www.poetryfoundation.org/poems/58767/the-tree-frog" target="_blank">C. Dale Young</a>
-					</p>
-					<div className="post-meta">
-						2 Feb 2018 (20 points) <a href="#">Upvote</a> - <a href="#">Downvote</a> - <a href="/post">View Post</a> - 12 Comments
-					</div>
-				</article>
-				<article className="post">
-					<h2>A Cat</h2>
-					<p className="post-content">
-						<img src="/img/toad.png" />
-					</p>
-					<div className="post-meta">
-						1 Feb 2018 (-11 points) <a href="#">Upvote</a> - <a href="#">Downvote</a> - <a href="/post">View Post</a> - 1 Comment
-					</div>
-				</article>
-				<article className="post">
-					<h2>Glorious</h2>
-					<p className="post-content">
-						<img src="/img/frog.png" />
-					</p>
-					<div className="post-meta">
-						31 Jan 2018 (41 points) <a href="#">Upvote</a> - <a href="#">Downvote</a> - <a href="/post">View Post</a> - 2 Comments
-					</div>
-				</article>
+        {Object.values(this.props.posts)
+          .filter(post => !post.deleted)
+          .map(post =>
+
+            <article className="post">
+              <h2>{ post.title }</h2>
+              <p className="post-content">
+                { post.body }
+              </p>
+              <p className="post-meta">
+                { post.timestamp } ({ post.voteScore } points)- <a href="#">Downvote</a> - <a href="#">Upvote</a> - <a href="#">View Post</a> - 12 comments
+              </p>
+            </article>)
+        }
 			</section>
 
     )
