b67de0cdb352908058ea955667dcbd30c0f05fae
Jenner Hanni <jenner@wickerbox.net>
2018-02-16 15:24:56 -0800

    bug: Unable to redirect after Post Delete

--- frontend/src/components/PostView.js
+++ frontend/src/components/PostView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getAllPosts, voteOnPost, deletePost } from '../actions'
 
@@ -9,34 +9,35 @@ class PostView extends Component {
   }
 
   render () {
-    return (
 
-       <section id="content">
-         {Object.values(this.props.posts)
-           .filter(post => post.id === this.props.match.params.post_id)
+      return (
 
-           .map(post =>
+         <section id="content">
+           {Object.values(this.props.posts)
+             .filter(post => post.id === this.props.match.params.post_id)
 
-             <article className="post" key="{ post.id }">
-               <h2>{ post.title }</h2>
-               <p className="post-content">
-                 { post.body }
-               </p>
-               <p className="post-content">
-                 Comments here
-               </p>
-               <p className="post-meta">
-                 <span>{ new Date(post.timestamp).toDateString() }</span>
-                 <span>({ post.voteScore } points)</span>
-                 <button onClick={() => this.props.vote(post.id, 'downVote')}>Downvote</button>
-                 <button onClick={() => this.props.vote(post.id, 'upVote')}>Upvote</button>
-                 <span><a href={"/post/" + post.id + "/edit"}>Edit Post</a></span>
-                 <button onClick={() => this.props.removePost(post.id)}>Delete</button>
-               </p>
-             </article>)
-         }
-       </section>
-    )
+             .map(post =>
+
+               <article className="post" key="{ post.id }">
+                 <h2>{ post.title }</h2>
+                 <p className="post-content">
+                   { post.body }
+                 </p>
+                 <p className="post-content">
+                   Comments here
+                 </p>
+                 <p className="post-meta">
+                   <span>{ new Date(post.timestamp).toDateString() }</span>
+                   <span>({ post.voteScore } points)</span>
+                   <button onClick={() => this.props.vote(post.id, 'downVote')}>Downvote</button>
+                   <button onClick={() => this.props.vote(post.id, 'upVote')}>Upvote</button>
+                   <span><a href={"/post/" + post.id + "/edit"}>Edit Post</a></span>
+                   <button onClick={() => this.props.removePost(post.id)}>Delete</button>
+                 </p>
+               </article>)
+           }
+         </section>
+      )
   }
 }
 
@@ -49,8 +50,9 @@ const mapDispatchToProps = dispatch => ({
   updatePosts: () => dispatch(getAllPosts()),
   vote: (postID, voteString) =>
     dispatch(voteOnPost(postID, voteString)),
-  removePost: (postID) =>
-    dispatch(deletePost(postID))
+  removePost: (postID) => {
+    dispatch(deletePost(postID));
+  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostView)
